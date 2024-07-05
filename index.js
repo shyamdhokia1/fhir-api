9ae2b44b989af57104528515c98b9179c60264c0
shyamdhokia1 <shyamdhokia123@hotmail.co.uk>
2024-07-05 15:05:04 +0000

    feat: initialise patient profile

---
+++ index.js
@@ -0,0 +1,21 @@
+const FHIRServer = require('@asymmetrik/node-fhir-server-core');
+const path = require('path');
+const { VERSIONS } = FHIRServer.constants;
+
+let config = {
+    server: {},
+    logging: {
+        level: 'debug'
+    },
+    profiles: {
+        patient: {
+            service: path.posix.resolve('./patient.service.js'),
+            versions: [ VERSIONS['3_0_1'] ]
+        }
+    }
+};
+let server = FHIRServer.initialize(config);
+
+server.listen(3000, () => {
+	server.logger.info('Starting the FHIR Server at localhost:3000');
+});
---
+++ patient.service.js
@@ -0,0 +1,3 @@
+module.exports.search = (args, logger) => new Promise((resolve, reject) => {
+	reject(new Error('Unable to locate patients'));
+});
