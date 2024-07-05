38f9d375a5a3ebbc74eb0c1fd2c562c3683b2fac
shyamdhokia1 <shyamdhokia123@hotmail.co.uk>
2024-07-05 17:19:46 +0000

    feat: query database

---
+++ database/index.js
@@ -0,0 +1,28 @@
+import * as dotenv from "dotenv";
+dotenv.config({path: "../.env"});
+
+import pg from 'pg';
+
+//Use connection pool
+const { Pool } = pg;
+const pool = new Pool();
+
+const query = async(query, params) => {
+    //initialise client here so can always access in finally block
+    let client = null
+    try {
+        client = await pool.connect()
+        const res = await client.query(query, params) //params is an array
+        console.log(res.rows)
+        return res.rows
+    } catch (err) {
+        console.error(`Error querying database: ${err}`);
+        return err
+    } finally {
+        if (client){
+            client.release()
+        }
+    }
+};
+
+query("SELECT * FROM patients WHERE identifier = $1", [11111111])
